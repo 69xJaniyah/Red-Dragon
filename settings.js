212b4dd5c39dc180da5dc26e9ecb75d2d6b42eb9
69xJaniyah <121703144+69xJaniyah@users.noreply.github.com>
2023-04-11 06:28:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -33,7 +33,7 @@ global.vcardowner = ['+94765743034'] // owner number
 global.ownername = "janiya" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
-global.location = "Sri lanka, Uva, Monaragala" // location
+global.location = "Sri lanka" // location
 
 //bot body 
 global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
